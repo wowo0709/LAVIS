1ffe568f87097ac3df44dcaf00d7bbe0770edc9f
wowo0709 <wjsduddn0709@naver.com>
2024-02-20 20:48:12 +0900

    [DEV] Define 'image_id' in eval dataset

--- lavis/datasets/datasets/text2shape_datasets.py
+++ lavis/datasets/datasets/text2shape_datasets.py
@@ -17,4 +17,33 @@ from lavis.datasets.datasets.caption_datasets import CaptionDataset, CaptionEval
 
 
 Text2ShapeDataset = CaptionDataset
-Text2ShapeEvalDataset = CaptionEvalDataset
+
+class Text2ShapeEvalDataset(CaptionEvalDataset):
+    def __init__(self, vis_processor, text_processor, vis_root, ann_paths):
+        """
+        vis_root (string): Root directory of images (e.g. coco/images/)
+        ann_root (string): directory to store the annotation file
+        split (string): val or test
+        """
+        super().__init__(vis_processor, text_processor, vis_root, ann_paths)
+
+    def __getitem__(self, index):
+        ann = self.annotation[index]
+
+        image_path = os.path.join(self.vis_root, ann["image"])
+        image = Image.open(image_path).convert("RGB")
+
+        image = self.vis_processor(image)
+
+        synset_id, model_id, _, image_file_name = ann["image"].split("/")
+        if "depth" in image_file_name:
+            idx = image_file_name.index("depth") + 5
+            img_id = "_".join(["text2shape", synset_id, model_id, image_file_name[:idx]])
+        else: # "rgb"
+            img_id = "_".join(["text2shape", synset_id, model_id, image_file_name, "rgb"])
+
+        return {
+            "image": image,
+            "image_id": img_id,
+            "instance_id": ann["instance_id"],
+        }
