82b09181ab2af53969d9ac499d668bfb6c643467
wowo0709 <wjsduddn0709@naver.com>
2024-03-12 22:17:30 +0900

    [REFACT] Remove redundant lines

--- lavis/common/config.py
+++ lavis/common/config.py
@@ -51,8 +51,6 @@ class Config:
         runner_config_validator.validate(runner_config)
 
     def _build_opt_list(self, opts):
-        print("===== opt =====")
-        print(opts)
         opts_dot_list = self._convert_to_dot_list(opts)
         return OmegaConf.from_dotlist(opts_dot_list)
 
